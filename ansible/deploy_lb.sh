9ab7e614f635ca41e4bf16ac8fea32460b69f2b9
Daniel Meißner <meise@koeln.ccc.de>
2016-12-25 15:20:31 +0100

    lb/config: improved deploy script and added current variables

--- ansible/deploy_lb.sh
+++ ansible/deploy_lb.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [[ $* != *--deploy* ]]; then
+  echo "Generate loadbalancer config and deploy it."
+  echo
+  echo "Usage: $0 --deploy"
+  exit 1
+fi
+
 BASEDIR=$(dirname "$0")
 
 function get_relay_json() {
@@ -7,12 +14,20 @@ function get_relay_json() {
 }
 
 function create_lb_cariables() {
-  perl $BASEDIR/register2lb.pl $BASEDIR/relays.json
+  file_pattern="# DO NOT EDIT MANUALLY BELOW THAT LINE"
 
+  # remove current variables
+  sed -ie "/${file_pattern}/Q" $BASEDIR/group_vars/loadbalancers
+  # insert matching pattern line
+  echo $file_pattern >> $BASEDIR/group_vars/loadbalancers
+  # write config
+  perl $BASEDIR/register2lb.pl $BASEDIR/relays.json >> $BASEDIR/group_vars/loadbalancers
+  echo "Pushed changes into $BASEDIR/group_vars/loadbalancers:"
+  cat  $BASEDIR/group_vars/loadbalancers
 }
 
 function deploy_lbs() {
-  echo "TODO: add code to deploy replays"
+  ansible-playbook $BASEDIR/site.yml -u $USER -s -i $BASEDIR/event -l 'loadbalancers' --tags haproxy_deploy
 }
 
 get_relay_json
