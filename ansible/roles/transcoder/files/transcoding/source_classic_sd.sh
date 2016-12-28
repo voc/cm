4514602262eb7824cef2171439cee0947050b625
Anton Schubert <anton.schubert@gmx.de>
2016-12-28 19:24:37 +0100

    roles/transcoder: use crf with maxrate for transcoding

--- ansible/roles/transcoder/files/transcoding/source_classic_sd.sh
+++ ansible/roles/transcoder/files/transcoding/source_classic_sd.sh
@@ -6,7 +6,7 @@
 
 ffmpeg -y -i "rtmp://${RTMP_SERVER}/stream/${STREAM}" -aspect 16:9 -threads:0 0 \
 	-fflags +genpts -flags +global_header \
-	-map 0:v:0 -c:v libvpx -g 75 -keyint_min 75 -deadline realtime -b:v 400k \
+	-map 0:v:0 -c:v libvpx -g 75 -keyint_min 75 -deadline realtime -b:v 400k -crf 10 \
 	-ac 1 -c:a libopus -vbr:a off -b:a 32k \
 	-map 0:a:0 -filter:a:0 pan=mono:c0=FL \
 	-map 0:a:0 -filter:a:1 pan=mono:c0=FR \
