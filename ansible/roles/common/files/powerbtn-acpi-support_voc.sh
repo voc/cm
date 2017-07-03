52eb4625193f5bcea53ca3e35f9e27426319cfbf
Florian Zeitz <florob@babelmonkeys.de>
2017-07-03 22:54:25 +0200

    powerbtn-acpi: Set LC_NUMERIC=C to ensure working argument parsing

--- ansible/roles/common/files/powerbtn-acpi-support_voc.sh
+++ ansible/roles/common/files/powerbtn-acpi-support_voc.sh
@@ -5,7 +5,7 @@
 
 TMP_DIR="/tmp"
 
-pressed_buttons=$(find $TMP_DIR -name 'power_button_press_*' -type f -cmin -0.083 | wc -l)
+pressed_buttons=$(LC_NUMERIC="C" find $TMP_DIR -name 'power_button_press_*' -type f -cmin -0.083 | wc -l)
 
 if [ $pressed_buttons -lt 3 ]; then
   touch $TMP_DIR/power_button_press_$(expr $pressed_buttons + 1)
