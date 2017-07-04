edecdc51f192e904ad53314faa55cdf361dbcb0c
Daniel Meißner <meise@koeln.ccc.de>
2017-07-04 21:39:27 +0200

    deploy.sh: ask for basic auth relay register password

--- ansible/deploy_relays.sh
+++ ansible/deploy_relays.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 if [[ $* != *--deploy* && $* != *--diff* ]]; then
   echo "Generate relay configs and deploy it."
   echo
@@ -7,26 +8,32 @@ if [[ $* != *--deploy* && $* != *--diff* ]]; then
   exit 1
 fi
 
+if [ -z "${PASSWORD}" ]; then
+  echo -n "Relay register voc password: "
+  read -r PASSWORD
+  echo $PASSWORD
+fi
+
 if [[ $* = *--diff* ]]; then
   DIFF=true
 fi
 
-BASEDIR=$(dirname "$0")
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function get_relay_json() {
-  wget -O $BASEDIR/relays.json https://voc:…@c3voc.de/33c3/register/relays
+  wget -O $DIR/relays.json https://voc:${PASSWORD}@c3voc.de/33c3/register/relays
 }
 
 function deploy_relays() {
   if [[ $DIFF = true ]]; then
-    ansible-playbook $BASEDIR/site.yml -f 1 -u $USER -s -i $BASEDIR/relays -l 'edges' --tags icecast,nginx,iptables --check --diff
+    $DIR/ansible-playbook-keepass $BASEDIR/site.yml -f 1 -u $USER -s -i $BASEDIR/relays -l 'edges' --tags icecast,nginx,iptables --check --diff
   else
     echo
     echo "Deploy new config on relays? [yes|no]"
     read choice
 
     if [ "$choice" = "yes" ]; then
-      ansible-playbook $BASEDIR/site.yml -f 1 -u $USER -s -i $BASEDIR/relays -l 'edges' --tags icecast,nginx,iptables --diff
+      $DIR/ansible-playbook-keepass $BASEDIR/site.yml -f 1 -u $USER -s -i $BASEDIR/relays -l 'edges' --tags icecast,nginx,iptables --diff
     else
       echo "Nothing deployed."
     fi
