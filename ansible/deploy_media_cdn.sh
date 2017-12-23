cd950bb41e963e6b8ae0f86aee8ed6b21643bb14
Mario Manno <mario.manno@gmail.com>
2017-12-23 14:05:48 +0000

    Add media mirror for berlin-ak

--- ansible/deploy_media_cdn.sh
+++ ansible/deploy_media_cdn.sh
@@ -2,6 +2,6 @@
 
 [ -d roles/ansible-acmetool ] || git clone https://github.com/artefactual-labs/ansible-acmetool.git roles/
 
-ansible-playbook --become -u voc -K -i media media-cdn.yml -v $*
-# TODO deploy media mirrors once roles are tested
-# ansible-playbook -u mm -i media media-mirror.yml -v $*
+# TODO voc user should have a sudo password and use -K?
+# TODO replace -e "@media.secrets with keepass
+ansible-playbook --become -u voc -i media  -e '@media.secrets' media.yml -v $*
