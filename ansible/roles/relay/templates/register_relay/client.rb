4e4983a8b31943946c9ace4ef5ddf30f7e932202
Anton Schubert <ischluff@mailbox.org>
2019-12-23 02:14:16 +0100

    relays: fix relay register client

--- ansible/roles/relay/templates/register_relay/client.rb
+++ ansible/roles/relay/templates/register_relay/client.rb
@@ -1,31 +1,38 @@
+#!/usr/bin/env ruby
 # encoding: utf-8
 
 require 'json'
 require 'base64'
 require 'openssl'
 require 'net/http'
+require 'net/https'
 
 # AES encryption helper
-def encrypt(data, password, iv)
+def encrypt(data, password)
   cipher = OpenSSL::Cipher.new('aes-256-cbc')
   cipher.encrypt # set cipher to be encryption mode
   cipher.key = Digest::SHA256.digest(password)
-  cipher.iv = iv
+  iv = cipher.random_iv
 
   encrypted = ''
   encrypted << cipher.update(data)
   encrypted << cipher.final
-  Base64.encode64(encrypted).gsub(/\n/, '')
+  base64_data = Base64.encode64(encrypted).gsub(/\n/, '')
+
+  {
+    iv: iv.unpack('H*')[0],
+    data: base64_data
+  }.to_json
 end
 
 # Please change me…
-uri            = URI('http://c3voc.de/relayregister/register')
-api_key        = '{{ lookup("keepass", "ansible/relay-register/33c3_api_key.password") }}'
-encryption_key = '{{ lookup("keepass", "ansible/relay-register/33c3_encryption_key.password") }}'
+uri            = URI('https://c3voc.de/relayregister/register')
+api_key        = '{{ lookup("keepass", "ansible/relay-register/api_key.password") }}'
+encryption_key = '{{ lookup("keepass", "ansible/relay-register/encryption_key.password") }}'
 
 # Create some useful variables for encryption and communication
-iv             = OpenSSL::Cipher.new('aes-256-cbc').random_iv.unpack('H*')[0]
 client         = Net::HTTP.new(uri.host, uri.port)
+client.use_ssl = true
 request        = Net::HTTP::Post.new(uri.path)
 
 # Gather raw data from system
@@ -42,10 +49,7 @@ raw_data = {
 }.to_json
 
 # Prepare request body
-data = {
-  iv: iv,
-  data: encrypt(raw_data, encryption_key, iv)
-}.to_json
+data = encrypt(raw_data, encryption_key)
 
 # Send data to server
 request.content_type = 'application/json'
