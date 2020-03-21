3f549b1b3ad6677cf5646bf1beab0b0e7e553b0a
Mario Manno <mario.manno@gmail.com>
2020-03-21 20:24:02 +0100

    media: cdn master update

---
+++ ansible/deploy_media_app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "$KEEPASS_PW" ]; then
+  echo "need keepass pw"
+  exit 1
+fi
+
+env KEEPASS="$KEEPASS" KEEPASS_PW="$KEEPASS_PW" $(command -v  python) lookup_plugins/keepass.py
+env KEEPASS="$KEEPASS" KEEPASS_PW="$KEEPASS_PW" ansible-playbook -i media media-app.yml -v --ssh-extra-args="-A" $*
