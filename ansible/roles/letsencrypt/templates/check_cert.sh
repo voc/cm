7a1e3fbfa2eeaec18f4d065c10d46d479a9ff147
Anton Schubert <ischluff@mailbox.org>
2020-09-11 19:51:28 +0200

    letsencrypt: fix cert check for hosts with multiple domains

---
+++ ansible/roles/letsencrypt/templates/check_cert.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+check_cert () {
+  res="$(/usr/lib/nagios/plugins/check_http -H {{ domain }} -C 20,13)"
+  if [ "0" -ne "$?" ]; then
+    send_mqtt_message "error" "system/cert/${TRUNC_HOSTNAME}" "<red>${res}</red>"
+  fi
+  debug_output "cert" "{{ domain }}" "${res}"
+}
+
+check_cert
