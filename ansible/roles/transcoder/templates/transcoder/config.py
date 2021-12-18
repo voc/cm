49e128ab060f06bc804cf164f22705d120f97320
Lukas Schauer <lukas@schauer.dev>
2021-12-19 00:29:35 +0100

    transcode consul-ting

--- ansible/roles/transcoder/templates/transcoder/config.py
+++
@@ -1,7 +0,0 @@
-icecast_password = "{{ lookup('keepass', 'ansible/icecast/icedist/source.password') }}"
-mqtt_broker = "{{ mqtt.server }}"
-mqtt_port = 8883
-mqtt_user = "{{ mqtt.username }}"
-mqtt_pass = "{{ mqtt.password }}"
-mqtt_certs = "/etc/ssl/certs/ca-certificates.crt"
-mqtt_host = "{{ ansible_hostname }}"
