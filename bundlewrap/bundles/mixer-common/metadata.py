a35c38cd9adc641141f3939bb09beb42a0fb1b5a
hexchen <hexchen@lilwit.ch>
2022-06-16 13:15:41 +0000

    bundles/mixer-common: add apt packages

--- bundlewrap/bundles/mixer-common/metadata.py
+++ bundlewrap/bundles/mixer-common/metadata.py
@@ -1,9 +1,22 @@
 import bwkeepass as keepass
 
 defaults = {
+    'apt': {
+        'packages': {
+            'dwm-voc': {},
+            'fonts-roboto': {},
+            'irrsi': {},
+            'runit': {},
+            'slim': {},
+            'x11vnc': {},
+            'x11-xserver-utils': {},
+            'xserver-xorg': {},
+            'xterm': {},
+        },
+    },
     'users': {
         'mixer': {
-            'password': keepass.password(['ansible', 'logins', 'mixer']),
+            'password': repo.vault.human_password_for(f'mixer@{node.name}'),
         },
     },
 }
