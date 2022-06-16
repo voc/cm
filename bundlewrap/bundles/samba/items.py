10be2d1f48e4c485d5d9fdc984f1375c28099c6a
Sophie Schiller <sophie.schiller01@gmail.com>
2022-06-16 11:03:12 +0200

    bundlewrap: wip samba bundle

---
+++ bundlewrap/bundles/samba/items.py
@@ -0,0 +1,27 @@
+svc_systemd = {
+    'nmbd': {
+        'needs': ['pkg_apt:samba'],
+    },
+    'smbd': {
+        'needs': ['pkg_apt:samba'],
+    },
+}
+svc_restart = ['svc_systemd:nmbd:restart', 'svc_systemd:smbd:restart']
+
+pkg_apt = {
+    'samba': {
+        'installed': True,
+    },
+}
+
+files = {
+    '/etc/samba/smb.conf': {
+        'content_type': 'mako',
+        'triggers': svc_restart,
+    },
+}
+
+if node.metadata.get('samba', {}).get('create_samba_user', False):
+    users = {
+        'samba': {},
+    }
