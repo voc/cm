0462190bf39488c5425b3e21f3984cee36ebb8f1
Franziska Kunsmann <hi@kunsmann.eu>
2022-07-09 10:32:09 +0200

    remove event-generator, document how to use subgroups

--- bundlewrap/groups/rooms.py
+++ bundlewrap/groups/rooms.py
@@ -1,6 +1,6 @@
 # rooms with minions
 for i in (1, 2, 3, 4, 5, 6, 80):
-    groups[f'rooms{i}'] = {
+    groups[f'saal{i}'] = {
         'members': {
             f'encoder{i}',
             f'mixer{i}',
@@ -16,7 +16,7 @@ for i in (1, 2, 3, 4, 5, 6, 80):
 
 # rooms without minions
 for i in (81, 191):
-    groups[f'rooms{i}'] = {
+    groups[f'saal{i}'] = {
         'members': {
             f'encoder{i}',
             f'mixer{i}',
