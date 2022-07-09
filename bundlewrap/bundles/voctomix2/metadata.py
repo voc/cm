7fb1725bbaceaaf1708ee947702ef243ae86f966
Franziska Kunsmann <hi@kunsmann.eu>
2022-07-09 14:31:39 +0200

    move voctomix2 dependencies to bundle:voctomix2

---
+++ bundlewrap/bundles/voctomix2/metadata.py
@@ -0,0 +1,22 @@
+
+defaults = {
+    'apt': {
+        'packages': {
+            'gir1.2-gst-plugins-base-1.0': {},
+            'gir1.2-gstreamer-1.0': {},
+            'gstreamer1.0-plugins-bad': {},
+            'gstreamer1.0-plugins-base': {},
+            'gstreamer1.0-plugins-good': {},
+            'gstreamer1.0-plugins-ugly': {},
+            'gstreamer1.0-tools': {},
+            'gstreamer1.0-vaapi': {},
+            'i965-va-driver-shaders': {},
+            'libgstreamer1.0-0': {},
+            'python3-gi': {},
+            'python3-pyinotify': {},
+            'python3-scipy': {},
+            'python3-sdnotify': {},
+            'rlwrap': {},
+        },
+    },
+}
