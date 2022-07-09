f37b89a5b9c22db8bb6b5db12efb0ea28f1f225f
Franziska Kunsmann <hi@kunsmann.eu>
2022-07-09 10:24:47 +0200

    hooks/encoders_test_voctocore_config: fix regex

--- bundlewrap/hooks/encoders_test_voctocore_config.py
+++ bundlewrap/hooks/encoders_test_voctocore_config.py
@@ -77,7 +77,7 @@ GSTREAMER_SUPPORTED_FORMATS = {
 
 def node_apply_start(repo, node, interactive=False, **kwargs):
     for sname, sconfig in node.metadata.get('voctocore/sources', {}).items():
-        if not sname == 'slides' and not re.match(r'^cam[0-1]+$', sname):
+        if not sname == 'slides' and not re.match(r'^cam[0-9]+$', sname):
             raise BundleError(f'{node.name}: voctocore source {sname} has invalid name, must be either "slides" or match "cam[0-9]+"')
 
         if not sconfig['devicenumber'].isdigit():
