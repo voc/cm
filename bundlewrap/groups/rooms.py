37b33a2e24f4639b8419975850d8d6dead40b4a6
Franziska Kunsmann <hi@kunsmann.eu>
2022-07-10 12:56:49 +0200

    groups/rooms: improve room generation

--- bundlewrap/groups/rooms.py
+++ bundlewrap/groups/rooms.py
@@ -1,10 +1,23 @@
-# rooms with minions
-for i in (1, 2, 3, 4, 5, 6, 80):
+ROOMS_WITHOUT_MINIONS = (
+    81,
+    191,
+)
+
+ROOMS_WITH_MINIONS = (
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    80,
+)
+
+for i in ROOMS_WITH_MINONS + ROOMS_WITHOUT_MINIONS:
     groups[f'saal{i}'] = {
         'members': {
             f'encoder{i}',
             f'mixer{i}',
-            f'minion{i}',
         },
         'metadata': {
             'event': {
@@ -17,20 +30,5 @@ for i in (1, 2, 3, 4, 5, 6, 80):
         },
     }
 
-# rooms without minions
-for i in (81, 191):
-    groups[f'saal{i}'] = {
-        'members': {
-            f'encoder{i}',
-            f'mixer{i}',
-        },
-        'metadata': {
-            'event': {
-                'room_name': f'Saal {i}',
-                'room_number': i
-            },
-            'voctocore': {
-                'streaming_endpoint': f's{i}',
-            },
-        },
-    }
+    if i in ROOMS_WITH_MINONS:
+        groups[f'saal{i}']['members'].add(f'minion{i}')
