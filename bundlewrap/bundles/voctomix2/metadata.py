234b7c1c76ff1f2070905cc14b87d840c7d38cf0
Franziska Kunsmann <hi@kunsmann.eu>
2022-07-22 15:59:08 +0200

    bundles/voctomix2: only install dependent packages if we need them

--- bundlewrap/bundles/voctomix2/metadata.py
+++ bundlewrap/bundles/voctomix2/metadata.py
@@ -1,24 +1,25 @@
-defaults = {
-    'apt': {
-        'packages': {
-            'gir1.2-gst-plugins-base-1.0': {},
-            'gir1.2-gstreamer-1.0': {},
-            'gstreamer1.0-plugins-bad': {},
-            'gstreamer1.0-plugins-base': {},
-            'gstreamer1.0-plugins-good': {},
-            'gstreamer1.0-plugins-ugly': {},
-            'gstreamer1.0-tools': {},
-            'gstreamer1.0-vaapi': {},
-            'gstreamer1.0-x': {},
-            'libgstreamer1.0-0': {},
-            'python3-gi': {},
-            'python3-pyinotify': {},
-            'python3-scipy': {},
-            'python3-sdnotify': {},
-            'rlwrap': {},
+if node.has_any_bundle(['voctocore', 'voctogui']):
+    defaults = {
+        'apt': {
+            'packages': {
+                'gir1.2-gst-plugins-base-1.0': {},
+                'gir1.2-gstreamer-1.0': {},
+                'gstreamer1.0-plugins-bad': {},
+                'gstreamer1.0-plugins-base': {},
+                'gstreamer1.0-plugins-good': {},
+                'gstreamer1.0-plugins-ugly': {},
+                'gstreamer1.0-tools': {},
+                'gstreamer1.0-vaapi': {},
+                'gstreamer1.0-x': {},
+                'libgstreamer1.0-0': {},
+                'python3-gi': {},
+                'python3-pyinotify': {},
+                'python3-scipy': {},
+                'python3-sdnotify': {},
+                'rlwrap': {},
+            },
         },
-    },
-}
+    }
 
 
 @metadata_reactor.provides(
