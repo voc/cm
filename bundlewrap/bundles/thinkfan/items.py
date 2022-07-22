9acc90e6a3d663af3d7faf9f4a300ebda011e9a3
Franziska Kunsmann <hi@kunsmann.eu>
2022-07-22 17:46:39 +0200

    bundles/thinkfan: adjust fan curve and logging

--- bundlewrap/bundles/thinkfan/items.py
+++ bundlewrap/bundles/thinkfan/items.py
@@ -12,6 +12,14 @@ files['/etc/modprobe.d/thinkfan.conf'] = {
     'content': 'options thinkpad_acpi fan_control=1\n',
 }
 
+files['/etc/systemd/system/thinkfan.service.d/bundlewrap.conf'] = {
+    'source': 'override.conf',
+    'triggers': {
+        'action:systemd-reload',
+        'svc_systemd:thinkfan:restart',
+    },
+}
+
 svc_systemd['thinkfan'] = {
     'needs': {
         'file:/etc/thinkfan.conf',
