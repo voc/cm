61d48977a6417e01e8e872ba3ada00097e257385
Franziska Kunsmann <hi@kunsmann.eu>
2022-07-22 15:37:38 +0200

    bundles/voctocore: move scripts directory to bundle:voctomix2

--- bundlewrap/bundles/voctomix2/items.py
+++ bundlewrap/bundles/voctomix2/items.py
@@ -1,5 +1,10 @@
 directories['/opt/voctomix2/release'] = {}
+
 git_deploy['/opt/voctomix2/release'] = {
     'repo': 'https://c3voc.de/git/voctomix',
     'rev': 'voctomix2',
+}
+
+directories['/opt/voctomix2/scripts'] = {
+    'purge': True,
 }
