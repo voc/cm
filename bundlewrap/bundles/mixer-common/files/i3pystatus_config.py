43d6ff5c34109b1761325d30b41d1fda130f1df6
Franziska Kunsmann <hi@kunsmann.eu>
2022-07-26 13:45:11 +0200

    bundles/mixer-common: use i3pystatus, because that supports click events

---
+++ bundlewrap/bundles/mixer-common/files/i3pystatus_config.py
@@ -0,0 +1,40 @@
+from i3pystatus import Status
+
+status = Status(
+    logfile='/dev/null',
+)
+
+status.register("backlight",
+    format="BACKLIGHT: {percentage}%",
+    backlight="*",
+    on_leftclick="sudo /usr/local/sbin/brightness full",
+    on_rightclick="sudo /usr/local/sbin/brightness half",
+    interval=1)
+
+status.register("clock",
+    format="%a, %Y-%m-%d %H:%M:%S (W%V)",)
+
+status.register("battery",
+    battery_ident="BAT0",
+    format="BAT0: {status}{percentage:.2f}% {remaining:%E%h:%M}",
+    status={
+        "DIS": "↓",
+        "CHR": "↑",
+        "FULL": "",
+    })
+
+status.register("temp",
+    format="CPU: {temp:.0f}°C",
+    hints={"markup": "pango"},
+    dynamic_color=True)
+
+status.register("network",
+    interface="eth0",
+    format_up="{interface}:[ {v6cidr}][ {v4cidr}]",
+    format_down="{interface}: down",
+    divisor=1024,
+    on_leftclick=None,
+    on_rightclick=None,
+    hints={"markup":"pango"})
+
+status.run()
