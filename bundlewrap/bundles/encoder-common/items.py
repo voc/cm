19293c34fbd20dd661c153dc4b81dd3b236038f2
Franziska Kunsmann <hi@kunsmann.eu>
2022-08-06 13:51:20 +0200

    bundles/encoder-common: do not require event acronym unless voctocore is used

--- bundlewrap/bundles/encoder-common/items.py
+++ bundlewrap/bundles/encoder-common/items.py
@@ -1,14 +1,25 @@
 from bundlewrap.exceptions import BundleError
 
 event = node.metadata.get('event/acronym', '')
-if not event:
+if not event and node.has_bundle('voctocore'):
     raise BundleError(f'{node.name} bundle:encoder-common requires event/acronym to be set, is "{event}".')
 
+directories[f'/video'] = {
+    'owner': 'voc',
+    'group': 'voc',
+}
+
 for path in ('capture', 'encoded', 'tmp', 'intros', 'fuse'):
-    directories[f'/video/{path}/{event}'] = {
-        'needed_by': {
-            'bundle:voctocore',
-        },
+    directories[f'/video/{path}'] = {
         'owner': 'voc',
         'group': 'voc',
     }
+
+    if event:
+        directories[f'/video/{path}/{event}'] = {
+            'needed_by': {
+                'bundle:voctocore',
+            },
+            'owner': 'voc',
+            'group': 'voc',
+        }
