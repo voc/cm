cdd66419a42bece04107dc8506de4073cb2bbb55
Lukas Schauer <lukas@schauer.dev>
2022-09-01 01:55:27 +0200

    use voctomix main branch for now (voctomix2 was b0rked on bullseye)

--- bundlewrap/bundles/voctomix2/items.py
+++ bundlewrap/bundles/voctomix2/items.py
@@ -2,7 +2,7 @@ directories['/opt/voctomix2/release'] = {}
 
 git_deploy['/opt/voctomix2/release'] = {
     'repo': 'https://c3voc.de/git/voctomix',
-    'rev': 'voctomix2',
+    'rev': 'main',
 }
 
 directories['/opt/voctomix2/scripts'] = {
