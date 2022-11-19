bda6ff9ea724bed006e8de33f229e01b07f2f9c7
Sophie Schiller <sophie.schiller01@gmail.com>
2022-11-19 11:08:19 +0100

    maintenance-weekend groups

---
+++ bundlewrap/groups/maintenance.py
@@ -0,0 +1,30 @@
+groups['maint-enc'] = {
+    'members': {
+        'encoder1',
+        'encoder2',
+        'encoder3',
+        'encoder4',
+        'encoder5',
+        'encoder6',
+    },
+}
+groups['maint-min'] = {
+    'members': {
+        'minion1',
+        'minion2',
+        'minion3',
+        'minion4',
+        'minion5',
+        'minion6',
+    },
+}
+groups['maint-mix'] = {
+    'members': {
+        'mixer1',
+        'mixer2',
+        'mixer3',
+        'mixer4',
+        'mixer5',
+        'mixer6',
+    },
+}
