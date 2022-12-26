2cc7978c7c0a57519d33b0421cfd732e127e6a78
Franziska Kunsmann <hi@kunsmann.eu>
2022-12-26 16:14:31 +0100

    bundles/mqtt-monitoring: only print ac_online status once per run

--- bundlewrap/bundles/mqtt-monitoring/files/plugins/ac_power.sh
+++ bundlewrap/bundles/mqtt-monitoring/files/plugins/ac_power.sh
@@ -1,6 +1,7 @@
 if [ -r "/sys/class/power_supply/AC/online" ]
 then
     ac_online=$(cat /sys/class/power_supply/AC/online)
+    voc2mqtt -t 'hosts/'$TRUNC_HOSTNAME'/stats/ac_online' -m $ac_online
 
     for battery in /sys/class/power_supply/BAT*
     do
@@ -14,7 +15,6 @@ then
         fi
 
         voc2mqtt -t 'hosts/'$TRUNC_HOSTNAME'/stats/'$identifier'_capacity' -m $capacity
-        voc2mqtt -t 'hosts/'$TRUNC_HOSTNAME'/stats/ac_online' -m $ac_online
 
         if [ "0" -eq "$ac_online" ] && [ "$capacity" -le 30 ]
         then
