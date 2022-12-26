a93b4e9a12915a79bac6dcc90563784096603dd1
Franziska Kunsmann <hi@kunsmann.eu>
2022-12-26 15:57:41 +0100

    bundles/mqtt-monitoring: monitor all batteries

--- bundlewrap/bundles/mqtt-monitoring/files/plugins/ac_power.sh
+++ bundlewrap/bundles/mqtt-monitoring/files/plugins/ac_power.sh
@@ -1,18 +1,24 @@
 if [ -r "/sys/class/power_supply/AC/online" ]
 then
     ac_online=$(cat /sys/class/power_supply/AC/online)
-    if [ -r "/sys/class/power_supply/BAT0/capacity" ]
-    then
-      bat0_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-    else
-      bat0_capacity="-1"
-    fi
 
-    voc2mqtt -t 'hosts/'$TRUNC_HOSTNAME'/stats/BAT0_capacity' -m $bat0_capacity
-    voc2mqtt -t 'hosts/'$TRUNC_HOSTNAME'/stats/ac_online' -m $ac_online
+    for battery in /sys/class/power_supply/BAT*
+    do
+        identifier="$(basename "$battery")"
 
-    if [ "0" -eq "$ac_online" ] && [ "$bat0_capacity" -le 30 ]
-    then
-        send_mqtt_message "error" "system/power/${TRUNC_HOSTNAME}" "<red>AC power offline! Battery $bat0_capacity % left.</red>"
-    fi
+        if [ -r "$battery/capacity" ]
+        then
+          capacity="$(cat "$battery/capacity")"
+        else
+          capacity="-1"
+        fi
+
+        voc2mqtt -t 'hosts/'$TRUNC_HOSTNAME'/stats/'$identifier'_capacity' -m $capacity
+        voc2mqtt -t 'hosts/'$TRUNC_HOSTNAME'/stats/ac_online' -m $ac_online
+
+        if [ "0" -eq "$ac_online" ] && [ "$capacity" -le 30 ]
+        then
+            send_mqtt_message "error" "system/power/${TRUNC_HOSTNAME}/$identifier" "<red>AC power offline! Battery $identifier $capacity % left.</red>"
+        fi
+    done
 fi
