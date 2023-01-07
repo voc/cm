e76bb65c0bc57433665a1ea0aab36ace9088acb9
Franziska Kunsmann <hi@kunsmann.eu>
2023-01-07 20:02:12 +0100

    move mqtt-monitoring stuff to `common` folder

--- bundlewrap/bundles/mqtt-monitoring/files/alert_shutdown.sh
+++ bundlewrap/bundles/mqtt-monitoring/files/alert_shutdown.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-MESSAGE="$(jq \
-    --null-input \
-    --arg name "$(hostnamectl --static)" \
-    --compact-output \
-    '{"name": $name}')"
-
-for i in 1 2 3 ; do
-    voc2mqtt \
-        -t "/voc/shutdown" \
-        -m "$MESSAGE" && break
-done
+../../../../common/mqtt-monitoring/alert_shutdown.sh
