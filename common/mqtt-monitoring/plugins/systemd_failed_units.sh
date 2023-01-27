693a8dca22d0a008da775a9b05c9e9bf7408f38e
Franziska Kunsmann <hi@kunsmann.eu>
2023-01-27 17:15:31 +0100

    common/mqtt-monitoring: remove trailing ^ from message

--- common/mqtt-monitoring/plugins/systemd_failed_units.sh
+++ common/mqtt-monitoring/plugins/systemd_failed_units.sh
@@ -2,6 +2,6 @@ res="$(LANG=C systemctl list-units --state=failed --no-legend --no-pager | sed '
 if [ -n "${res}" ]; then
     echo "${res}" | while read line; do
         service="$(echo "${line}" | awk '{print $1}')"
-        voc2alert "error" "systemd/${service}" "systemd unit failed^"
+        voc2alert "error" "systemd/${service}" "systemd unit failed"
     done
 fi
