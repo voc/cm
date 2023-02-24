a6b982d4701bb014f136533cb14d21d23f0d5f1f
Franziska Kunsmann <hi@kunsmann.eu>
2023-02-24 15:47:56 +0100

    bw/bundles/systemd-networkd: support bonding network interfaces

--- bundlewrap/bundles/systemd-networkd/items.py
+++ bundlewrap/bundles/systemd-networkd/items.py
@@ -44,12 +44,48 @@ directories = {
 
 
 for interface, config in node.metadata.get('interfaces').items():
+    if node.metadata.get(f'systemd-networkd/bonds/{interface}', {}):
+        continue
+
     files[f'/etc/systemd/network/{interface}.network'] = {
         'source': 'template-iface.network',
         'content_type': 'mako',
         'context': {
             'interface': interface,
             'config': config,
+        },
+        'needed_by': {
+            'svc_systemd:systemd-networkd',
+        },
+        'triggers': {
+            'svc_systemd:systemd-networkd:restart',
+        },
+    }
+
+for bond, config in node.metadata.get('systemd-networkd/bonds', {}).items():
+    files[f'/etc/systemd/network/{bond}.netdev'] = {
+        'source': 'template-bond.netdev',
+        'content_type': 'mako',
+        'context': {
+            'bond': bond,
+            'mode': config.get('mode', '802.3ad'),
+            'prio': config.get('priority', '32768'),
+        },
+        'needed_by': {
+            'svc_systemd:systemd-networkd',
+        },
+        'triggers': {
+            'svc_systemd:systemd-networkd:restart',
+        },
+    }
+
+    files[f'/etc/systemd/network/{bond}.network'] = {
+        'source': 'template-bond.network',
+        'content_type': 'mako',
+        'context': {
+            'bond': bond,
+            'match': config['match'],
+            'vlans': node.metadata.get(f'interfaces/{bond}/vlans', set()),
         },
         'needed_by': {
             'svc_systemd:systemd-networkd',
--- bundlewrap/bundles/systemd-networkd/metadata.py
+++ bundlewrap/bundles/systemd-networkd/metadata.py
@@ -7,3 +7,23 @@ defaults = {
         },
     },
 }
+
+
+@metadata_reactor.provides(
+    'interfaces',
+)
+def add_vlan_infos_to_interface(metadata):
+    interfaces = {}
+
+    for iface in metadata.get('interfaces', {}):
+        if '.' not in iface:
+            continue
+
+        interface,vlan = iface.split('.')
+
+        interfaces.setdefault(interface, {}).setdefault('vlans', set())
+        interfaces[interface]['vlans'].add(vlan)
+
+    return {
+        'interfaces': interfaces,
+    }
