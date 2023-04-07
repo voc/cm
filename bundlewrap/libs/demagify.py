fe0272fcb3b07a9cb61b432ff95a3b7f6bf99eed
Franziska Kunsmann <hi@kunsmann.eu>
2023-04-07 22:05:16 +0200

    bw: encoder1 uses srt now

--- bundlewrap/libs/demagify.py
+++ bundlewrap/libs/demagify.py
@@ -1,18 +1,20 @@
 import bwkeepass as keepass
 
-def demagify(something):
+def demagify(something, vault):
     if isinstance(something, str):
         if something.startswith('!kee_user:'):
             return keepass.username(something[10:])
         elif something.startswith('!kee_pass:'):
             return keepass.password(something[10:])
+        elif something.startswith('!decrypt:'):
+            return vault.decrypt(something[9:])
         return something
     elif isinstance(something, dict):
-        return {k:demagify(v) for k,v in something.items()}
+        return {k:demagify(v, vault) for k,v in something.items()}
     elif isinstance(something, list):
-        return [demagify(i) for i in something]
+        return [demagify(i, vault) for i in something]
     elif isinstance(something, set):
-        return {demagify(i) for i in something}
+        return {demagify(i, vault) for i in something}
     elif isinstance(something, tuple):
-        return (demagify(i) for i in something)
+        return (demagify(i, vault) for i in something)
     return something
