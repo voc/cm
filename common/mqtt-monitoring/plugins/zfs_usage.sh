651199bf16642f1f707e78a52239250a68235468
Franziska Kunsmann <hi@kunsmann.eu>
2023-04-15 09:45:46 +0200

    common/mqtt-monitoring: improve zfs usage information

--- common/mqtt-monitoring/plugins/zfs_usage.sh
+++ common/mqtt-monitoring/plugins/zfs_usage.sh
@@ -3,7 +3,14 @@ IFS=$'\n'
 
 for line in $(zfs list -d1 -Ho name,used | sort -k1 -h)
 do
-    voc2alert "info" "zfs/usage" "$line"
+    path="$(echo "$line" | awk '{print $1}')"
+    diskspace="$(zfs get -Hp used "$path" | awk '{print $3}')"
+
+    # only alert if there is more than 1GB used
+    if [[ "$diskspace" -gt 1073741824 ]]
+    then
+        voc2alert "info" "zfs" "$(printf '%7s %s' "$(echo "$diskspace / 1073741824" | bc)G" "$path")"
+    fi
 done
 
 IFS=$OLDIFS
