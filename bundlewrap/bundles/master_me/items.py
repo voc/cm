0eb508eb85e379ea7e65c7bf807be9598a210659
Sophie Schiller <sophie.schiller01@gmail.com>
2023-06-09 17:56:05 +0200

    wip

--- bundlewrap/bundles/master_me/items.py
+++ bundlewrap/bundles/master_me/items.py
@@ -1,8 +1,9 @@
 directories['/usr/local/lib/ladspa'] = {}
 
 files['/usr/local/lib/ladspa/master_me.so'] = {
-        'mode': '0755',
-        'needs': [
-            'pkg_apt:ffmpeg',
-        ]
-    }
+    'mode': '0755',
+    'needs': [
+        'pkg_apt:ffmpeg',
+    ],
+    'content_type': 'binary',
+}
