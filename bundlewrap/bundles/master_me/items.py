079558f28e59f5be078bb27b0d8a514453e469d0
Sophie Schiller <sophie.schiller01@gmail.com>
2023-06-09 17:54:49 +0200

    vim

--- bundlewrap/bundles/master_me/items.py
+++ bundlewrap/bundles/master_me/items.py
@@ -1,5 +1,8 @@
 directories['/usr/local/lib/ladspa'] = {}
 
 files['/usr/local/lib/ladspa/master_me.so'] = {
-        'mode': '0700',
+        'mode': '0755',
+        'needs': [
+            'pkg_apt:ffmpeg',
+        ]
     }
