36a3604536145b8effdbc5b22d3c6dad46095388
Sophie Schiller <sophie.schiller01@gmail.com>
2023-06-09 13:29:28 +0200

    enable audio in fb playout

--- bundlewrap/bundles/voctocore/files/decklink-playout.sh
+++ bundlewrap/bundles/voctocore/files/decklink-playout.sh
@@ -11,13 +11,16 @@ ffmpeg \
     -y \
     -i tcp://localhost:${port} \
 % if device == 'fb':
+    -filter_complex "[0:a]pan=stereo|c0=c0|c1=c1,volume=0.8[s_pgm]" \
     -map 0:v \
 % endif
     -c:v rawvideo \
 % if device == 'fb':
     -pix_fmt bgra \
     -f fbdev \
-    /dev/fb0
+    /dev/fb0 \
+    -map "[s_pgm]" \
+    -f alsa hw:0,7
 % else:
     -c:a pcm_s16le \
     -pix_fmt uyvy422 \
