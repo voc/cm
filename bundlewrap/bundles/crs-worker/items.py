1d08be03d518378e8abee0583464a8b3e857ff57
Franziska Kunsmann <hi@kunsmann.eu>
2023-08-05 13:51:43 +0200

    bw/bundles/crs-worker: support more than one encoding worker per system

--- bundlewrap/bundles/crs-worker/items.py
+++ bundlewrap/bundles/crs-worker/items.py
@@ -15,10 +15,6 @@ WORKER_SCRIPTS = {
         'secret': 'meta',
         'script': 'script-C-cut-postprocessor.pl',
     },
-    'encoding': {
-        'secret': 'encoding',
-        'script': 'script-D-encoding.pl',
-    },
     'postencoding': {
         'secret': 'meta',
         'script': 'script-E-postencoding-auphonic.pl',
@@ -36,11 +32,18 @@ WORKER_SCRIPTS = {
     }
 }
 
-i = 2
-if node.metadata.get("hostname").split(".")[0] in ["minion128-1", "minion128-2"]:
-    for _ in range(5):
-        WORKER_SCRIPTS[f"encoding{i}"] = WORKER_SCRIPTS["encoding"].copy()
-        i = i+1
+number_of_workers = node.metadata.get('crs-worker/number_of_encoding_workers')
+if number_of_workers > 1:
+    for i in range(number_of_workers):
+        WORKER_SCRIPTS[f'encoding{i}'] = {
+            'secret': 'encoding',
+            'script': 'script-D-encoding.pl',
+        }
+else:
+    WORKER_SCRIPTS['encoding'] = {
+        'secret': 'encoding',
+        'script': 'script-D-encoding.pl',
+    }
 
 directories['/opt/crs-scripts'] = {}
 git_deploy['/opt/crs-scripts'] = {
