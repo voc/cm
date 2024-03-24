6499b709fabd45eb596f82622999b5b261b38ebb
Franziska Kunsmann <hi@kunsmann.eu>
2024-03-24 11:37:15 +0100

    bw: add group for mikrotik switches

---
+++ bundlewrap/groups/switches.py
@@ -0,0 +1,12 @@
+groups['switches-mikrotik'] = {
+    'bundles': {
+        'routeros',
+    },
+    'metadata': {
+        'routeros': {
+            'gateway': '10.73.0.254',
+        },
+    },
+    'os': 'routeros',
+    'username': 'admin',
+}
