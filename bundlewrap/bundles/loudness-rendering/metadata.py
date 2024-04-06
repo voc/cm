89d2f10d204b58a4954b93f5a391151add358522
Franziska Kunsmann <hi@kunsmann.eu>
2024-04-06 09:06:14 +0200

    bw/bundles/loudness-rendering: autogenerate from encoder list

--- bundlewrap/bundles/loudness-rendering/metadata.py
+++ bundlewrap/bundles/loudness-rendering/metadata.py
@@ -1,3 +1,5 @@
+from bundlewrap.exceptions import BundleError
+
 defaults = {
     'apt': {
         'packages': {
@@ -5,3 +7,25 @@ defaults = {
         },
     },
 }
+
+
+@metadata_reactor.provides(
+    'loudness-rendering/streams',
+)
+def from_encoders(metadata):
+    streams = {}
+    for encoder in metadata.get('loudness-rendering/from_encoders', set()):
+        rnode = repo.get_node(encoder)
+        if not rnode.has_bundle('voctocore'):
+            raise BundleError(f'{node.name}: loudness rendering requested for {encoder}, but is missing bundle:voctocore')
+        endpoint = rnode.metadata.get('voctocore/streaming_endpoint')
+        streams[encoder] = {
+            'source': f'https://cdn.c3voc.de/hls/{endpoint}/native_hd.m3u8',
+            'room': rnode.metadata.get('event/room_name'),
+            'output': f'rtmp://ingest2.c3voc.de/relay/{endpoint}_loudness',
+        }
+    return {
+        'loudness-rendering': {
+            'streams': streams,
+        },
+    }
