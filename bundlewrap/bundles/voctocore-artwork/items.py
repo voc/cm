479b8accd6f89b24f90a2a1c4a3871f0bf9c4139
Franziska Kunsmann <hi@kunsmann.eu>
2024-04-12 12:23:14 +0200

    bw/bundles/voctocore-artwork: make usable even when bundle:voctocore is not deployed

--- bundlewrap/bundles/voctocore-artwork/items.py
+++ bundlewrap/bundles/voctocore-artwork/items.py
@@ -1,10 +1,6 @@
 from bundlewrap.exceptions import BundleError
 from os.path import exists, join
 
-assert node.has_bundle('voctocore')
-
-room_number = node.metadata.get('room_number', 0)
-event_slug = node.metadata.get('event/slug')
 
 directories['/opt/voc/share'] = {
     'purge': True,
@@ -42,72 +38,76 @@ actions['voctocore-artwork_update_schedule_and_overlays'] = {
     },
 }
 
-for target_file, possible_sources in {
-    '/opt/voc/share/overlay_hd.png': [
-        join(event_slug, f'saal{room_number}', 'overlay_hd.png'),
-        join(event_slug, 'overlay_hd.png'),
-        'default-overlay-hd.png',
-    ],
-    '/opt/voc/share/overlay_slides.png': [
-        join(event_slug, f'saal{room_number}', 'overlay_slides.png'),
-        join(event_slug, 'overlay_slides.png'),
-        'default-overlay-slides.png',
-    ],
-    '/opt/voc/share/bgloop.ts': [
-        join(event_slug, f'saal{room_number}', 'bgloop.ts'),
-        join(event_slug, 'bgloop.ts'),
-        'default-bgloop.ts',
-    ],
-    '/opt/voc/share/bg.png': [
-        join(event_slug, f'saal{room_number}', 'bg.png'),
-        join(event_slug, 'bg.png'),
-        'default-bg.png',
-    ],
-    '/opt/voc/share/bg_lec.png': [
-        join(event_slug, f'saal{room_number}', 'bg_lec.png'),
-        join(event_slug, 'bg_lec.png'),
-        'default-bg_lec.png',
-    ],
-    '/opt/voc/share/bg_lecm.png': [
-        join(event_slug, f'saal{room_number}', 'bg_lecm.png'),
-        join(event_slug, 'bg_lecm.png'),
-        'default-bg_lecm.png',
-    ],
-    '/opt/voc/share/bg_sbs.png': [
-        join(event_slug, f'saal{room_number}', 'bg_sbs.png'),
-        join(event_slug, 'bg_sbs.png'),
-        'default-bg_sbs.png',
-    ],
-    '/opt/voc/share/pause.ts': [
-        join(event_slug, f'saal{room_number}', 'pause.ts'),
-        join(event_slug, 'pause.ts'),
-        'default-pause.ts',
-    ],
-    '/opt/voc/share/nostream.ts': [
-        join(event_slug, f'saal{room_number}', 'nostream.ts'),
-        join(event_slug, 'nostream.ts'),
-        'default-nostream.ts',
-        join(event_slug, f'saal{room_number}', 'nostream.png'),
-        join(event_slug, 'nostream.png'),
-        'default-nostream.png',
-    ],
-    '/opt/voc/share/pause-music.mp3': [
-        join(event_slug, f'saal{room_number}', 'pause-music.mp3'),
-        join(event_slug, 'pause-music.mp3'),
-        'default-pause-music.mp3',
-    ],
-}.items():
-    source_file = None
+if node.has_bundle('voctocore'):
+    room_number = node.metadata.get('room_number', 0)
+    event_slug = node.metadata.get('event/slug')
 
-    for source in possible_sources:
-        if exists(join(repo.path, 'data', 'voctocore-artwork', 'files', source)):
-            source_file = source
-            break
-    if source_file is not None:
-        files[target_file] = {
-            'source': source,
-            'content_type': 'binary',
-            'triggers': {
-                'svc_systemd:voctomix2-voctocore:restart',
-            },
-        }
+    for target_file, possible_sources in {
+        '/opt/voc/share/overlay_hd.png': [
+            join(event_slug, f'saal{room_number}', 'overlay_hd.png'),
+            join(event_slug, 'overlay_hd.png'),
+            'default-overlay-hd.png',
+        ],
+        '/opt/voc/share/overlay_slides.png': [
+            join(event_slug, f'saal{room_number}', 'overlay_slides.png'),
+            join(event_slug, 'overlay_slides.png'),
+            'default-overlay-slides.png',
+        ],
+        '/opt/voc/share/bgloop.ts': [
+            join(event_slug, f'saal{room_number}', 'bgloop.ts'),
+            join(event_slug, 'bgloop.ts'),
+            'default-bgloop.ts',
+        ],
+        '/opt/voc/share/bg.png': [
+            join(event_slug, f'saal{room_number}', 'bg.png'),
+            join(event_slug, 'bg.png'),
+            'default-bg.png',
+        ],
+        '/opt/voc/share/bg_lec.png': [
+            join(event_slug, f'saal{room_number}', 'bg_lec.png'),
+            join(event_slug, 'bg_lec.png'),
+            'default-bg_lec.png',
+        ],
+        '/opt/voc/share/bg_lecm.png': [
+            join(event_slug, f'saal{room_number}', 'bg_lecm.png'),
+            join(event_slug, 'bg_lecm.png'),
+            'default-bg_lecm.png',
+        ],
+        '/opt/voc/share/bg_sbs.png': [
+            join(event_slug, f'saal{room_number}', 'bg_sbs.png'),
+            join(event_slug, 'bg_sbs.png'),
+            'default-bg_sbs.png',
+        ],
+        '/opt/voc/share/pause.ts': [
+            join(event_slug, f'saal{room_number}', 'pause.ts'),
+            join(event_slug, 'pause.ts'),
+            'default-pause.ts',
+        ],
+        '/opt/voc/share/nostream.ts': [
+            join(event_slug, f'saal{room_number}', 'nostream.ts'),
+            join(event_slug, 'nostream.ts'),
+            'default-nostream.ts',
+            join(event_slug, f'saal{room_number}', 'nostream.png'),
+            join(event_slug, 'nostream.png'),
+            'default-nostream.png',
+        ],
+        '/opt/voc/share/pause-music.mp3': [
+            join(event_slug, f'saal{room_number}', 'pause-music.mp3'),
+            join(event_slug, 'pause-music.mp3'),
+            'default-pause-music.mp3',
+        ],
+    }.items():
+        source_file = None
+
+        for source in possible_sources:
+            if exists(join(repo.path, 'data', 'voctocore-artwork', 'files', source)):
+                source_file = source
+                break
+        if source_file is not None:
+            files[target_file] = {
+                'source': source,
+                'content_type': 'binary',
+                'triggers': {
+                    'svc_systemd:voctomix2-voctocore:restart',
+                },
+            }
