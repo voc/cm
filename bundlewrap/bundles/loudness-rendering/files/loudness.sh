3f00948f075c85fbcc6c06d71224daee972e33ea
Sophie Schiller <sophie.schiller01@gmail.com>
2024-05-30 18:26:13 +0200

    gpn22: loudness rendering

--- bundlewrap/bundles/loudness-rendering/files/loudness.sh
+++ bundlewrap/bundles/loudness-rendering/files/loudness.sh
@@ -36,8 +36,8 @@ ffmpeg \
         [a1] aformat=sample_fmts=fltp:sample_rates=48000:channel_layouts=stereo [audio];
         [base][v1] overlay=shortest=1 [tmp1];
         [tmp1][scaled] overlay=shortest=1:y=360 [ov];
-        [ov] drawtext='fontcolor=white:x=45:y=36:fontsize=50:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:textfile=/opt/loudness-rendering/data/${NAME}/line1.txt:reload=30' [ov1];
-        [ov1] drawtext='fontcolor=white:x=45:y=86:fontsize=20:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:textfile=/opt/loudness-rendering/data/${NAME}/line2.txt:reload=30' [ov2];
+        [ov] drawtext='fontcolor=white:x=45:y=36:fontsize=50:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:textfile=/opt/loudness-rendering/data/${NAME}/line1.txt:reload' [ov1];
+        [ov1] drawtext='fontcolor=white:x=45:y=86:fontsize=20:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:textfile=/opt/loudness-rendering/data/${NAME}/line2.txt:reload' [ov2];
         [ov2] drawtext='fontcolor=white:x=45:y=111:fontsize=20:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:text=${ROOM} - ${NAME}' [out]" \
     -map "[out]" -c:v libx264 -threads 2 -preset veryfast -x264-params keyint=30 -tune zerolatency -crf:0 26 -profile:0 high -level:0 4.1 -c:a aac -strict -2 -pix_fmt yuv420p \
     -map "[audio]" -c:a aac -b:a 128k \
