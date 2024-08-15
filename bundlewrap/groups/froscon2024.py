34c1d3af5accba3abbcef1fddf7095a737a9a25f
Lukas Schauer <lukas@schauer.dev>
2024-08-15 18:17:04 +0200

    froscon 2024

---
+++ bundlewrap/groups/froscon2024.py
@@ -0,0 +1,10 @@
+subgroups = ["saal1", "saal2", "saal3", "saal4", "saal5", "saalc4"]
+members = ["releasing"]
+
+[metadata.event]
+name = "FrOSCon 2024"
+slug = "froscon2024"
+timezone = "Europe/Berlin"
+acronym = "froscon2024"
+slogan = "Free and Open Source Software Conference"
+
