688192c0bf6d47e4b55fa98b129650f1fe1e9b72
kunsi <voc@kunsmann.eu>
2024-08-18 12:01:57 +0200

    bw/bundles/loudness-rendering: put room name in bigger letters at the bottom

--- bundlewrap/bundles/loudness-rendering/files/loudness.sh
+++ bundlewrap/bundles/loudness-rendering/files/loudness.sh
@@ -38,7 +38,8 @@ ffmpeg \
         [tmp1][scaled] overlay=shortest=1:y=360 [ov];
         [ov] drawtext='fontcolor=white:x=45:y=36:fontsize=50:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:textfile=/opt/loudness-rendering/data/${NAME}/line1.txt:reload=30' [ov1];
         [ov1] drawtext='fontcolor=white:x=45:y=86:fontsize=20:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:textfile=/opt/loudness-rendering/data/${NAME}/line2.txt:reload=30' [ov2];
-        [ov2] drawtext='fontcolor=white:x=45:y=111:fontsize=20:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:text=${ROOM} - ${NAME}' [out]" \
+        [ov2] drawtext='fontcolor=white:x=45:y=111:fontsize=20:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:text=${NAME}' [ov3];
+        [ov3] drawtext='fontcolor=white:x=45:y=303:fontsize=50:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf:text=${ROOM}' [out]" \
     -map "[out]" -c:v libx264 -threads 2 -preset veryfast -x264-params keyint=30 -tune zerolatency -crf:0 26 -profile:0 high -level:0 4.1 -strict -2 -pix_fmt yuv420p \
     -map "[audio]" -c:a aac -b:a 128k \
     -f flv \
