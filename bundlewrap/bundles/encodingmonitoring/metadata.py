b97d5e18bf7d0a93990d3169a81ca2994db2ae8a
Jenny Danzmayr <mail@evilscientress.de>
2025-03-28 13:41:41 +0100

    encodingmonitoring: use MQTT topic and interval from metadata

--- bundlewrap/bundles/encodingmonitoring/metadata.py
+++ bundlewrap/bundles/encodingmonitoring/metadata.py
@@ -8,6 +8,7 @@ defaults = {
         'host': 'mqtt.c3voc.de',
         'user': 'bundlewrap',
         'password': repo.vault.decrypt('encrypt$gAAAAABmk8KmZd6RTiomPYjrNyhGtd7zFFUcWVqeQNozzyhBO8cfIzihu5DczdRHy8HCneXgXA2eYNIEXvp_2561HEJzv7qWB5Tdxxt-ySAA8VUuZB4liqm3CO4gwRgBXUZxMnQYpLsx'),
-        'topic': '/voc/ffmpegprogress',
+        'topic': '/voc/ffmpeg/progress',
+        'interval': '5',
     },
 }
