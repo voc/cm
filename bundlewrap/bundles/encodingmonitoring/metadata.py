53804e9ef3ecf5d36d37f2f48b09fdbc6f26f04c
Sophie Schiller <sophie.schiller01@gmail.com>
2025-03-28 10:25:59 +0100

    feature: ffmpeg2mqtt

---
+++ bundlewrap/bundles/encodingmonitoring/metadata.py
@@ -0,0 +1,13 @@
+import bwkeepass as keepass
+
+defaults = {
+    'unit-status-on-login': {
+        'ffmpeg2mqtt',
+    },
+    'ffmpeg2mqtt': {
+        'host': 'mqtt.c3voc.de',
+        'user': 'bundlewrap',
+        'password': repo.vault.decrypt('encrypt$gAAAAABmk8KmZd6RTiomPYjrNyhGtd7zFFUcWVqeQNozzyhBO8cfIzihu5DczdRHy8HCneXgXA2eYNIEXvp_2561HEJzv7qWB5Tdxxt-ySAA8VUuZB4liqm3CO4gwRgBXUZxMnQYpLsx'),
+        'topic': '/voc/ffmpegprogress',
+    },
+}
