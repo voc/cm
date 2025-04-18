e54baddd2bae920ed5ecadc90757e5b7caf96f19
Sophie Schiller <sophie.schiller01@gmail.com>
2025-04-19 00:05:06 +0200

    bw/encodingmonitoring use correct topic

--- bundlewrap/bundles/encodingmonitoring/metadata.py
+++ bundlewrap/bundles/encodingmonitoring/metadata.py
@@ -8,7 +8,7 @@ defaults = {
         'host': repo.libs.defaults.mqtt_hostname,
         'user': repo.libs.defaults.mqtt_username,
         'password': repo.vault.decrypt(repo.libs.defaults.mqtt_password),
-        'topic': '/voc/ffmpeg/progress',
+        'topic': 'voc/ffmpeg/progress/{job}',
         'interval': '5',
     },
 }
