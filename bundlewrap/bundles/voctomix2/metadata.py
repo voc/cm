9b8d72730b4dd8edf006418ac35eda7f25e661f8
Anton Schubert <ischluff@mailbox.org>
2025-06-24 15:55:52 +0200

    use voctomix 2.2

--- bundlewrap/bundles/voctomix2/metadata.py
+++ bundlewrap/bundles/voctomix2/metadata.py
@@ -1,53 +1,53 @@
 DEBIAN_TO_VOCTOMIX_VERSION = {
-    10: 'voctomix2',
-    12: '2.3',
+    10: "voctomix2",
+    12: "2.2",
 }
 
-if node.has_any_bundle(['voctocore', 'voctogui']):
+if node.has_any_bundle(["voctocore", "voctogui"]):
     defaults = {
-        'apt': {
-            'packages': {
-                'gir1.2-gst-plugins-base-1.0': {},
-                'gir1.2-gstreamer-1.0': {},
-                'gstreamer1.0-plugins-bad': {},
-                'gstreamer1.0-plugins-base': {},
-                'gstreamer1.0-plugins-good': {},
-                'gstreamer1.0-plugins-ugly': {},
-                'gstreamer1.0-tools': {},
-                'gstreamer1.0-vaapi': {},
-                'gstreamer1.0-x': {},
-                'libgstreamer1.0-0': {},
-                'python3-gi': {},
-                'python3-pyinotify': {},
-                'python3-scipy': {},
-                'python3-sdnotify': {},
-                'rlwrap': {},
+        "apt": {
+            "packages": {
+                "gir1.2-gst-plugins-base-1.0": {},
+                "gir1.2-gstreamer-1.0": {},
+                "gstreamer1.0-plugins-bad": {},
+                "gstreamer1.0-plugins-base": {},
+                "gstreamer1.0-plugins-good": {},
+                "gstreamer1.0-plugins-ugly": {},
+                "gstreamer1.0-tools": {},
+                "gstreamer1.0-vaapi": {},
+                "gstreamer1.0-x": {},
+                "libgstreamer1.0-0": {},
+                "python3-gi": {},
+                "python3-pyinotify": {},
+                "python3-scipy": {},
+                "python3-sdnotify": {},
+                "rlwrap": {},
             },
         },
     }
 
 
 if node.os_version[0] in DEBIAN_TO_VOCTOMIX_VERSION:
-    defaults['voctomix2'] = {
-        'rev': DEBIAN_TO_VOCTOMIX_VERSION[node.os_version[0]],
+    defaults["voctomix2"] = {
+        "rev": DEBIAN_TO_VOCTOMIX_VERSION[node.os_version[0]],
     }
 
 
 @metadata_reactor.provides(
-    'voctomix2/version_tuple',
+    "voctomix2/version_tuple",
 )
 def voctomix_version(metadata):
-    rev = metadata.get('voctomix2/rev')
+    rev = metadata.get("voctomix2/rev")
 
-    if '.' in rev:
-        version = tuple([int(i) for i in rev.split('.')])
-    elif rev == 'voctomix2':
+    if "." in rev:
+        version = tuple([int(i) for i in rev.split(".")])
+    elif rev == "voctomix2":
         version = (2,)
     else:
         version = (2, 9999)
 
     return {
-        'voctomix2': {
-            'version_tuple': version,
+        "voctomix2": {
+            "version_tuple": version,
         },
     }
