461d338902b86fc387a805b6cdcece9514d6eb79
Molly Miller <c3voc@katzeiminternet.de>
2025-09-07 16:06:49 +0200

    loudness-player: add results from hardware scan on modula machine

--- nixos/hosts/loudness-player/default.nix
+++ nixos/hosts/loudness-player/default.nix
@@ -116,6 +116,7 @@ in
 {
   imports = [
     "${modulesPath}/installer/cd-dvd/iso-image.nix"
+    "${modulesPath}/installer/scan/not-detected.nix"
   ];
   config = {
 
@@ -134,7 +135,31 @@ in
     isoImage.splashImage = ./splash-legacy.png;
     isoImage.storeContents = lib.mkForce [ config.system.build.toplevel ] ;
 
-    system.stateVersion = "23.11"; # do not touch
+    system.stateVersion = "25.05"; # do not touch
+
+    # Results from hardware scan on Modula hardware
+    boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" "xfs" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+    fileSystems."/" =
+      { device = "/dev/disk/by-label/nixos";
+        fsType = "xfs";
+      };
+
+    fileSystems."/boot" =
+      { device = "/dev/disk/by-label/boot";
+        fsType = "vfat";
+        options = [ "fmask=0022" "dmask=0022" ];
+      };
+
+    # System configuration
 
     users.groups.voc = {};
     users.users.voc = {
