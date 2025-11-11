137d6c367ab280654633d81180bac26a34ad3a98
Franziska Kunsmann <hi@kunsmann.eu>
2025-11-11 13:35:04 +0100

    bw/bundles/snmp-temperature-monitoring: autogenerate monitoring for routeros devices

--- bundlewrap/bundles/snmp-temperature-monitoring/metadata.py
+++ bundlewrap/bundles/snmp-temperature-monitoring/metadata.py
@@ -5,3 +5,28 @@ defaults = {
         },
     },
 }
+
+
+@metadata_reactor.provides(
+    'snmp-temperature-monitoring/routeros',
+)
+def auto_routeros(metadata):
+    room = metadata.get('room_number', None)
+    if not room:
+        return {}
+
+    nodes = repo.nodes_in_all_groups([
+        f'saal{room}',
+        'switches-mikrotik',
+    ])
+    if not nodes:
+        return {}
+
+    return {
+        'snmp-temperature-monitoring': {
+            'routeros': {
+                rnode.hostname
+                for rnode in nodes
+            },
+        },
+    }
