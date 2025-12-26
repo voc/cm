4abf3f8d3cd4d63df194f82b2099847b037446a3
Dario Mehlich <d.mehlich@gmail.com>
2025-12-26 22:15:22 +0100

    nixos: added "aarch64-linux" to `flake.nix`

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -95,7 +95,7 @@
           };
         };
       };
-    } //  flake-utils.lib.eachSystem ([ "x86_64-linux" "x86_64-darwin" "aarch64-darwin"]) (system: let
+    } //  flake-utils.lib.eachSystem ([ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"]) (system: let
       pkgs = pkgs' system;
     in {
       devShells.default = pkgs.mkShell {
