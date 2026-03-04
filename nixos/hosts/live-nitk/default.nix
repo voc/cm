b20986b22750e4fd46d65604de915986d7d576bf
Lukas Schauer <lukas@schauer.dev>
2026-03-04 18:01:43 +0100

    live.nitk shouldn't be origin

--- nixos/hosts/live-nitk/default.nix
+++ nixos/hosts/live-nitk/default.nix
@@ -17,7 +17,7 @@ in
   ];
   config = {
     system.stateVersion = "25.11"; # do not touch
-    deployment.tags = [ "relays" "origin-relays" ];
+    deployment.tags = [ "relays" ];
 
     boot.loader.grub.enable = true;
     boot.loader.grub.device = "/dev/sda";
