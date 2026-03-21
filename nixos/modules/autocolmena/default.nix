2934eb714ec16c8629889c54b1ffaa8fd54fb213
Molly Miller <c3voc@katzeiminternet.de>
2026-03-21 17:41:28 +0100

    nixos: add module for automatic system upgrades with colmena
    This is based on the upstream NixOS system.autoUpgrade option, but
    uses colmena for building and applying the configuration instead of
    nixos-rebuild. Some of our host configs use options provided by
    colmena at evaluation-time, which means these hosts can't be expressed
    as vanilla nixosConfiguration flake outputs..

---
+++ nixos/modules/autocolmena/default.nix
@@ -0,0 +1,61 @@
+{ config, lib, pkgs, ... }:
+
+# like system.autoUpgrade but using colmena
+
+let
+  cfg = config.system.autoColmena;
+in
+{
+  options.system.autoColmena = {
+    enable = lib.mkEnableOption "autoColmena";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.colmena;
+      description = "Colmena package to use for the upgrade.";
+    };
+
+    flake = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      example = "github:voc/cm?dir=nixos";
+      description = "Flake URI of the Colmena configuration to build.";
+    };
+
+    dates = lib.mkOption {
+      type = lib.types.str;
+      default = "03:30";
+      example = "daily";
+      description = "systemd calendar spec for scheduling the upgrade unit.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = cfg.flake != null;
+        message = "The option 'system.autoColmena.flake' option must not be null.";
+      }
+    ];
+
+    # colmena requires explicit option to enable self-deployment.
+    deployment.allowLocalDeployment = true;
+
+    systemd.timers.colmena-upgrade = {
+      wantedBy = [ "timers.target" ];
+      timerConfig.OnCalendar = cfg.dates;
+    };
+
+    systemd.services.colmena-upgrade = rec {
+      description = "System upgrade with Colmena.";
+      after = [ "network-online.target" ];
+      wants = after;
+      restartIfChanged = false;
+      serviceConfig.Type = "oneshot";
+      path = [
+        config.nix.package.out
+      ];
+      script = "${lib.getExe' cfg.package "colmena"} -f '${cfg.flake}' apply-local -v switch";
+    };
+  };
+}
