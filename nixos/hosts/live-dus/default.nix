48591f06dda466b9717585d8fc04f002a898e937
Lukas Schauer <lukas@schauer.dev>
2026-03-25 11:22:25 +0100

    live.dus is not an origin relay

--- nixos/hosts/live-dus/default.nix
+++ nixos/hosts/live-dus/default.nix
@@ -17,14 +17,13 @@ in
   ];
   config = {
     system.stateVersion = "23.11"; # do not touch
-    deployment.tags = [ "relays" "origin-relays" ];
+    deployment.tags = [ "relays" "edge-relays" ];
 
     networking.hostName = lib.mkOverride 1 "live";
     networking.domain = "dus.c3voc.de";
 
     services.voc-relay = {
       enable = true;
-      isOrigin = true;
       addressv4 = "46.228.205.55";
       addressv6 = "2001:4ba0:92c1:9f:46:228:205:55";
     };
