16f5fd5de16b9544deb302c98972476451cc8339
Lukas Schauer <lukas@schauer.dev>
2026-03-27 12:13:33 +0100

    added upload-server module

---
+++ nixos/modules/upload-server/default.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+# upload-server module
+#
+# Provides an upload server for live stream snippets
+#
+let cfg = config.services.upload-server;
+  fqdn = config.networking.hostName + "." + config.networking.domain;
+in {
+  options = {
+    services.upload-server = {
+      enable = mkEnableOption "upload-server";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      stream-api
+    ];
+    sops.secrets = {
+      upload_server_config = {
+        sopsFile = ./secrets.yaml;
+        key = "config";
+      };
+    };
+    systemd.services.upload-server = {
+      serviceConfig = {
+        ExecStart = "${pkgs.stream-api}/bin/upload-server -config ${config.sops.secrets.upload_server_config.path}";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+}
+
