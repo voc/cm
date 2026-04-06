aec6b62abf6ef6ac6a1481d21fbfe69783f49f24
Lukas Schauer <lukas@schauer.dev>
2026-04-06 21:49:27 +0200

    ingest: enable icecast

--- nixos/modules/voc-ingest/default.nix
+++ nixos/modules/voc-ingest/default.nix
@@ -18,7 +18,7 @@ let
         - type: srtrelay
           url: http://localhost:8084
   '';
-  
+
 in
 {
   options = {
@@ -40,11 +40,102 @@ in
     services.srtrelay.enable = true;
     services.rtmp-auth.enable = true;
 
+    sops.templates."icecast.xml".owner = "nobody";
+    sops.templates."icecast.xml".content = ''
+      <icecast>
+          <hostname>${config.networking.hostName}.${config.networking.domain}</hostname>
+          <location>voc</location>
+          <admin>contact@c3voc.de</admin>
+          <server-id>Icecast 2</server-id>
+
+          <limits>
+              <clients>4096</clients>
+              <sources>500</sources>
+              <threadpool>10</threadpool>
+              <queue-size>4055040</queue-size>
+              <client-timeout>30</client-timeout>
+              <header-timeout>15</header-timeout>
+              <source-timeout>10</source-timeout>
+
+              <burst-on-connect>0</burst-on-connect>
+              <burst-size>524280</burst-size>
+          </limits>
+
+          <authentication>
+              <source-password>${config.sops.placeholder.icecastSourcePassword}</source-password>
+              <relay-password>${config.sops.placeholder.icecastRelayPassword}</relay-password>
+
+              <admin-user>admin</admin-user>
+              <admin-password>${config.sops.placeholder.icecastAdminPassword}</admin-password>
+          </authentication>
+
+          <listen-socket>
+              <port>8000</port>
+              <bind-address>::</bind-address>
+              <bindv6only>0</bindv6only>
+          </listen-socket>
+
+
+          <!-- additional mount points with separate authentication -->
+
+          <!-- This flag turns on the icecast2 fileserver from which static files can be served. -->
+          <fileserve>1</fileserve>
+          <paths>
+              <basedir>/usr/local/share/icecast</basedir>
+
+              <logdir>/var/log/icecast</logdir>
+              <webroot>${pkgs.icecast}/share/icecast/web</webroot>
+              <adminroot>${pkgs.icecast}/share/icecast/admin</adminroot>
+
+              <alias source="/" destination="/status.xsl"/>
+          </paths>
+
+          <logging>
+            <errorlog>error.log</errorlog>
+
+            <loglevel>3</loglevel> <!-- 4 Debug, 3 Info, 2 Warn, 1 Error -->
+            <logsize>1000</logsize>  <!-- Max size of a logfile -->
+          </logging>
+
+          <security>
+              <chroot>0</chroot>
+              <changeowner>
+                  <user>nobody</user>
+                  <group>nogroup</group>
+              </changeowner>
+          </security>
+      </icecast>
+    '';
     sops.secrets = {
       htpasswd = {
         sopsFile = ./secrets.yaml;
         key = "htpasswd";
         owner = "nginx";
+      };
+      icecastSourcePassword = {
+        sopsFile = ./secrets.yaml;
+        key = "icecast/sourcePassword";
+      };
+      icecastRelayPassword = {
+        sopsFile = ./secrets.yaml;
+        key = "icecast/relayPassword";
+      };
+      icecastAdminPassword = {
+        sopsFile = ./secrets.yaml;
+        key = "icecast/adminPassword";
+      };
+    };
+
+    systemd.services.icecast = {
+      after = [ "network.target" ];
+      description = "Icecast Network Audio Streaming Server";
+      wantedBy = [ "multi-user.target" ];
+
+      preStart = "mkdir -p /var/log/icecast && chown nobody:nogroup /var/log/icecast";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.icecast}/bin/icecast -c ${config.sops.templates."icecast.xml".path}";
+        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
       };
     };
 
