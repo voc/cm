0c156a6a31618bae277974e648cb015f782b7755
Julian Jacobi <mail@julianjacobi.net>
2026-04-13 17:49:03 +0200

    bw: Update bw in flake

--- bundlewrap/flake.nix
+++ bundlewrap/flake.nix
@@ -1,25 +1,32 @@
 {
   description = "c3voc bundlewrap";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils } @ inputs: flake-utils.lib.eachSystem ([
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachSystem ([
     "x86_64-linux"
     "x86_64-darwin"
     "aarch64-linux"
     "aarch64-darwin"
   ]) (system:
     let
-      pkgs = (import nixpkgs { inherit system; });
+      pkgsUnstable = (import inputs.nixpkgs-unstable {inherit system;});
+      pkgs = (import nixpkgs {
+        inherit system;
+        overlays = [(final: prev: {
+          bundlewrap = pkgsUnstable.bundlewrap;
+        })];
+      });
     in {
       legacyPackages = pkgs;
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           curl
           rsync
+          bundlewrap
           (python3.withPackages (py: [
-            py.bundlewrap
-
             py.virtualenv
           ]))
         ];
