b76ec78564258df790c1e0627681df2ae7d4366a
Anton Schubert <ischluff@mailbox.org>
2026-04-18 00:45:46 +0200

    nixos: add lb.wob

--- nixos/init-nixos.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh $1 "sudo systemctl stop consul nebula@nebula; sudo rm -r /var/lib/consul /etc/machine-id; sudo systemd-machine-id-setup; sudo nix-shell -p ssh-to-age --command 'cat /etc/ssh/ssh_host_ed25519_key.pub | ssh-to-age'"
